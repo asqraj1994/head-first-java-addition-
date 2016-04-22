d3947316399e086fb42f1da60215348d097fb003
HP <HP@asqraj>
2016-04-23 01:15:41 +0530

    new commit

---
+++ src/project/addition/ProjectAddition.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.addition;
+
+/**
+ *
+ * @author HP
+ */
+public class ProjectAddition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int first_number, second_number, answer;
+        first_number = 40;
+        second_number = 60;
+        answer = first_number + second_number;
+        System.out.println("Addition Total = " + answer);
+    }
+    
+}
